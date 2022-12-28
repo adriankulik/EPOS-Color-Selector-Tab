9ca42f00a72d91dde60c5b16b91f02e27b8dd9c6
adriankulik <adriankulik93@gmail.com>
2022-12-28 18:28:33 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+// may use MultiRangeSlider. Created by developergovindgupta. Licensed under the ISC license: https://opensource.org/licenses/ISC
 
 function App() {
+  // states code
+  const [opacity, setOpacity] = useState(100);
+  const [range, setRange] = useState(100);
+  const [color1, setColor1] = useState("#000000");
+  const [color2, setColor2] = useState("#ffffff");
+  // canvas (gradient preview code)
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    canvas.width = 300;
+    canvas.height = 50;
+    let gradient = context.createLinearGradient(0, 0, 200, 0);
+    gradient.addColorStop(0, color1);
+    gradient.addColorStop(1, color2);
+    context.fillStyle = gradient;
+    context.globalAlpha = opacity / 100;
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Color input</h1>
+      <canvas ref={canvasRef} />
+      <hr></hr>
+      <p>Opacity: {opacity}</p>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        defaultValue={opacity}
+        onChange={(e) => setOpacity(e.target.value)}
+      />
+      <p>Range: {range}</p>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        defaultValue={range}
+        onChange={(e) => setRange(e.target.value)}
+      />
+      <hr></hr>
+      <p>min</p>
+      <input
+        type="color"
+        defaultValue={color1}
+        onChange={(e) => setColor1(e.target.value)}
+      />
+      <p>max</p>
+      <input
+        type="color"
+        defaultValue={color2}
+        onChange={(e) => setColor2(e.target.value)}
+      />
     </div>
   );
 }
